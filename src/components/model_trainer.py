059d064f9b9e94139f163621d1c1ee73fb280224
edmon-ds <edwin.montilla.ds@gmail.com>
2024-10-08 13:18:34 -0500

    Prediction Pipeline

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -27,23 +27,14 @@ class ModelTrainerConfig:
 class ModelTrainer:
     def __init__(self):
         self.model_trainer_config = ModelTrainerConfig
-
-    def initiate_model_trainer(self , train_array , test_array ):
-        try:
-            logging.info("split training and test input data")
-            
-            X_train , y_train , X_test , y_test = (train_array[: , :-1] , 
-                                                    train_array[: , -1] , 
-                                                    test_array[: , :-1] , 
-                                                    test_array[: , -1]
-                                                    )
-            models = {
+        
+        self.models = {
                         "Decision Tree": DecisionTreeRegressor(),
                         "Elastic Net": ElasticNet(),  # Reemplaza LinearRegression con ElasticNet
                         "XGBRegressor": XGBRegressor(),        
                      }
 
-            params = {
+        self.params = {
                 "Decision Tree": {
                                     'max_depth': [None, 5, 10, 15, 20],  # Profundidad máxima
                                 },
@@ -56,34 +47,66 @@ class ModelTrainer:
                         'n_estimators': [8, 16]
                 }
                 }
+        self.best_model_name = ""
+        self.best_model_score = 0
 
+    def initiate_model_trainer(self , train_array , test_array ):
+        try:
+            logging.info("split training and test input data")
+            
+            X_train , y_train , X_test , y_test = (train_array[: , :-1] , 
+                                                    train_array[: , -1] , 
+                                                    test_array[: , :-1] , 
+                                                    test_array[: , -1]
+                                                    )
+            
 
             # Adaptar la lista de modelos solo para los que están en params
 
-
-            model_report:dict = evaluate_models(X_train = X_train , y_train = y_train  ,
-                                             X_test = X_test  , y_test = y_test , models = models , param=params)
+            model_report:dict = self.evaluate_models(X_train = X_train , y_train = y_train  ,
+                                             X_test = X_test  , y_test = y_test ,
+                                             models = self.models , param= self.params)
              
-            best_model_name = max(model_report , key = model_report.get)
-            best_model_score = model_report[best_model_name]
-            best_model = models[best_model_name]
+            self.best_model_name = max(model_report , key = model_report.get)
+
+            best_model = self.models[self.best_model_name]
+            self.best_model_score = model_report[self.best_model_name]
             
-            if best_model_score < 0.6:
+            
+            if self.best_model_score < 0.6:
                 raise CustomException("No best model found")
             
             logging.info("best found model on both training and testing dataset")
             save_object(file_path = self.model_trainer_config.trained_model_file_path ,obj = best_model)
 
-            predicted = best_model.predict(X_test)
-            r2_s = r2_score(y_test , predicted)
-            return r2_s
+            #predicted = best_model.predict(X_test)
+            #r2_s = r2_score(y_test , predicted)
+            #return r2_s
         
         except Exception as e:
             raise CustomException(e , sys)
         
-        
-            
+    def evaluate_models(self , X_train, y_train,X_test,y_test,models,param):
+        try:
+            report = {}
 
+            for i in range(len(list(models))):
+                model = list(models.values())[i]
+                para = param[list(models.keys())[i]]
 
+                gs = GridSearchCV(model,para,cv=3)
+                gs.fit(X_train,y_train)
 
+                model.set_params(**gs.best_params_)
+                model.fit(X_train,y_train)
 
+                y_test_pred = model.predict(X_test)
+
+                test_model_score = r2_score(y_test, y_test_pred)
+
+                report[list(models.keys())[i]] = test_model_score
+
+            return report
+
+        except Exception as e:
+            raise CustomException(e, sys)
---
+++ src/pipeline/train_pipeline.py
@@ -0,0 +1,32 @@
+from src.components.data_ingestion import * 
+from src.components.data_transformation import * 
+from src.components.model_trainer import * 
+
+if __name__ =="__main__":
+    print("iniciando")
+    print()
+    
+    print("creando ruta de datasets")
+    print()
+    obj = DataIngestion()
+    train_data_path , test_data_path = obj.initiate_data_ingestion()
+    
+    print("transformando datasets")
+    print()
+    data_transformation = DataTransformation()
+    train_arr , test_arr , _ = data_transformation.initiate_data_transformation(train_data_path , test_data_path )
+    
+    print("entrenando varios modelos")
+    print()
+    
+    modeltrainer = ModelTrainer()
+    modeltrainer.initiate_model_trainer(train_arr , test_arr)
+    print("el modelo ha sido entrenado y elegido")
+    print()
+    print("nombre y puntuacion del mejor modelo")
+    print(f"nombre: {modeltrainer.best_model_name} puntuacion : {modeltrainer.best_model_score}")
+   
+   
+    
+    print("proceso finalizado")
+    print()
